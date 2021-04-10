5134248c14c9f9c45aaff964179770e3a9008f72
unknown <38404108+prasunroy@users.noreply.github.com>
2021-04-10 17:00:44 +0530

    Added utilities

---
+++ utils/estimate_keypoints.py
@@ -0,0 +1,55 @@
+import argparse
+import csv
+import cv2
+import glob
+import numpy as np
+import os
+from openpose.body.estimator import BodyPoseEstimator
+
+
+class KeypointEstimator(object):
+    
+    def __init__(self):
+        self.estimator = BodyPoseEstimator(pretrained=True)
+    
+    def _write_csv(self, path, mode, data):
+        with open(path, mode, newline='') as fp:
+            writer = csv.writer(fp)
+            writer.writerow(data)
+    
+    def _get_column_labels(self, num_keypoints=18):
+        column_labels = ['file_id', 'img_h', 'img_w']
+        for i in range(num_keypoints):
+            column_labels.extend([f'p{i}_x', f'p{i}_y'])
+        return column_labels
+    
+    def estimate_keypoints(self, fp, img_dir, verbose=False):
+        out_dir = os.path.dirname(fp)
+        if out_dir and not os.path.isdir(out_dir):
+            os.makedirs(out_dir)
+        self._write_csv(fp, 'w', self._get_column_labels(18))
+        images = sorted(glob.glob(f'{img_dir}/**/*.*', recursive=True))
+        for i, image in enumerate(images):
+            x = cv2.imread(image)
+            if x is None:
+                continue
+            keypoints = self.estimator(x)
+            keypoints = keypoints[0] if len(keypoints) > 0 else np.zeros((18, 3), dtype=np.int32)
+            keypoints[np.where(keypoints[:, 2]==0), :2] = -1
+            keypoints = keypoints[:, :2].reshape(-1).tolist()
+            file_id = os.path.normpath(image)[len(os.path.normpath(img_dir)):]
+            file_id = os.path.splitext(file_id)[0].replace('/', '').replace('\\', '')
+            self._write_csv(fp, 'a', [file_id, x.shape[0], x.shape[1]] + keypoints)
+            if verbose:
+                print(f'\r[INFO] Estimating keypoints... {(i+1)*100.0/len(images):3.0f}%', end='')
+        if verbose:
+            print('\r[INFO] Estimating keypoints... 100%')
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-o', required=True, help='output file')
+    ap.add_argument('-i', required=True, help='image directory')
+    args = vars(ap.parse_args())
+    estimator = KeypointEstimator()
+    estimator.estimate_keypoints(args['o'], args['i'], verbose=True)
---
+++ utils/generate_posemaps.py
@@ -0,0 +1,34 @@
+import argparse
+import numpy as np
+import os
+import pandas as pd
+
+
+def generate_posemaps(out_dir, keypoints_data, verbose=False):
+    if out_dir and not os.path.isdir(out_dir):
+        os.makedirs(out_dir)
+    df = pd.read_csv(keypoints_data)
+    for i in range(len(df)):
+        file_id = str(df.iloc[i, 0])
+        img_h, img_w = np.uint32(df.iloc[i, 1:3])
+        keypoints = np.int32(df.iloc[i, 3:]).reshape(-1, 2)
+        posemap = np.zeros((img_h, img_w, keypoints.shape[0]), dtype=np.uint8)
+        for k in range(keypoints.shape[0]):
+            col, row = keypoints[k]
+            if row == -1 or col == -1:
+                continue
+            posemap[row, col, k] = 1
+        fp = f'{out_dir}/{file_id}.npz'
+        np.savez_compressed(fp, posemap)
+        if verbose:
+            print(f'\r[INFO] Generating posemaps... {(i+1)*100.0/len(df):3.0f}%', end='')
+    if verbose:
+        print('')
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-o', required=True, help='output directory')
+    ap.add_argument('-k', required=True, help='keypoints data')
+    args = vars(ap.parse_args())
+    generate_posemaps(args['o'], args['k'], verbose=True)
